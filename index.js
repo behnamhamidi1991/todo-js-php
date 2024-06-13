29566a6375a850aeb5a3882501b8f42b12678efd
behnamhamidi1991 <eslp91@hotmail.com>
2024-06-13 11:32:14 +0330

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+const todoForm = document.querySelector('#todo-form');
+const formInput = document.querySelector('#formInput');
+
+function addItem (e) {
+    e.preventDefault();
+
+    const newItem = formInput.value;
+
+    // Create an LI
+
+
+    // Create a P and Div
+
+
+    // Create Buttons
+    
+}
+
+
+
+
+// Event Listeners
+todoForm.addEventListener('submit', addItem)
