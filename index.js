748845718f0aca807fd9e07db226a17b9e38a62f
behnamhamidi1991 <eslp91@hotmail.com>
2024-06-18 11:39:26 +0330

    crud is done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById("form");
 const itemInput = document.getElementById("formInput");
 const itemList = document.getElementById("itemList");
+const clearAll = document.getElementById('clearAll');
 
 function addItem(e) {
   e.preventDefault();
@@ -40,6 +41,21 @@ function createBtnIcon (classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if(e.target.parentElement.classList.contains('deleteBtn')) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function removeAllItems () {
+  if (window.confirm('Are you sure you want to delete all items?')) {
+    while (itemList.firstChild) {
+      itemList.firstChild.remove();
+    }
+  }
+}
+
 // Event Listeners
 itemForm.addEventListener("submit", addItem);
-
+itemList.addEventListener('click', removeItem)
+clearAll.addEventListener('click', removeAllItems)
