fd351e0f3a3835d6418268075dfe04f82f8f415c
behnamhamidi1991 <eslp91@hotmail.com>
2024-06-18 11:21:11 +0330

    commit

--- index.js
+++ index.js
@@ -11,7 +11,35 @@ function addItem(e) {
     alert("You must add a task!");
     return;
   }
+
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
+
+  // Add button
+  const button = createBtn('deleteBtn');
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+  itemInput.value = '';
+}
+
+function createBtn (classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+
+  const icon = createBtnIcon('bx bxs-trash-alt')
+  button.appendChild(icon);
+
+  return button;
+}
+
+function createBtnIcon (classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+
+  return icon;
 }
 
 // Event Listeners
 itemForm.addEventListener("submit", addItem);
+
